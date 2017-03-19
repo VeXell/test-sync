7cc502f49b58a174f33d35f87c52c42ae0273174
vexell <vexell@gmail.com>
2017-03-19 14:25:29 +0300

    More examples

--- server.php
+++ server.php
@@ -2,9 +2,16 @@
 
 header('Access-Control-Allow-Origin: *');
 
-sleep(2);
+$SLEEP_TIME = 2;
+
+if (isset($_REQUEST['sw']) && !empty($_REQUEST['sw'])) {
+    $SLEEP_TIME = 0.5;
+}
+
+sleep($SLEEP_TIME);
 echo "ok";
 
 if (isset($_REQUEST['sw']) && !empty($_REQUEST['sw'])) {
-    file_put_contents('sw.txt', date('Y-m-d H:i:s') . '|' . file_get_contents('php://input') . PHP_EOL, FILE_APPEND | LOCK_EX);
+    // Uncomment if you want to see background sync after page unload
+    //file_put_contents('sw.txt', date('Y-m-d H:i:s') . '|' . file_get_contents('php://input') . PHP_EOL, FILE_APPEND | LOCK_EX);
 }
